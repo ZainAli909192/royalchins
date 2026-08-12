c11899279d9b32f13e35b4f415a453ab630f7130
linkplusae-dot <linkplus.ae@gmail.com>
2026-08-12 15:16:06 +0400

    Coming soon created

---
+++ app/comingsoon/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ComingSoonPage() {  
+    return (
+        <div className="coming-soon-container">
+            <img src="/comingsoon.svg" alt="Coming Soon" srcset="" />
+        </div>
+    );
+}
