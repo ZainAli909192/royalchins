80cc4942546026d77485f46bbd441df92e0059eb
linkplusae-dot <linkplus.ae@gmail.com>
2026-08-12 16:04:15 +0400

    Coming soon shoing on index page now

--- app/comingsoon/page.jsx
+++ app/comingsoon/page.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-export default function ComingSoonPage() {  
-    return (
-        <div className="coming-soon-container">
-            <img src="/comingsoon.svg" alt="Coming Soon" srcset="" />
-        </div>
-    );
+export default function ComingSoonPage() {
+  return (
+    <div className=" mt-[10%] w-full min-h-screen overflow-hidden bg-[#1c1d22]">
+      <img
+        src="/comingsoon.svg"
+        alt="Coming Soon"
+        className="block h-auto w-full object-cover"
+      />
+    </div>
+  );
 }
